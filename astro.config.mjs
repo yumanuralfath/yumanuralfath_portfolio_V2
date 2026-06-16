2873fe565e726bdfd4578a4a5cf59111be8f1c75
yumanuralfath <yumanuralfath2@gmail.com>
2026-06-16 10:38:58 +0700

    refactor: migrate to Tailwind CSS v4 and integrate Lucide icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://yumana.my.id",
+  compressHTML: true,
+  i18n: {
+    defaultLocale: "id",
+    locales: ["id", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      exclude: ["@tailwindcss/vite"]
+    }
+  },
+  build: {
+    inlineStylesheets: "auto",
+  },
+});
