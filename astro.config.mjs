a93aa8e4adf133321310e0cc82fa45c46d4dcab2
yumanuralfath <yumanuralfath2@gmail.com>
2026-06-16 11:36:06 +0700

    fix: cloudflare project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yumana.my.id",
+  adapter: cloudflare({
+    sessionKVBindingName: undefined,
+  }),
   compressHTML: true,
   i18n: {
     defaultLocale: "id",
@@ -16,8 +20,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
-      exclude: ["@tailwindcss/vite"]
-    }
+      exclude: ["@tailwindcss/vite"],
+    },
   },
   build: {
     inlineStylesheets: "auto",
